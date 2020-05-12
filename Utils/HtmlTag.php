4d23b765faaa2d27c6c689b388f349d7e36438ae
Christian Puchinger <puchinger@burda.com>
2020-05-12 03:25:18 +0200

    Extract tag funtionality to trait.

--- Utils/HtmlTag.php
+++ Utils/HtmlTag.php
@@ -6,15 +6,12 @@ use Exception;
 
 class HtmlTag extends HtmlAttributes {
 
+  use HtmlTagTrait;
+
   protected static $selfClosing = [
     'area', 'base', 'br', 'col', 'embed', 'hr', 'img', 'input',
     'link', 'meta', 'param', 'source', 'track', 'wbr'
   ];
-
-  /**
-   * @var string|null
-   */
-  private $tag;
 
   /**
    * HtmlTag constructor.
@@ -43,76 +40,11 @@ class HtmlTag extends HtmlAttributes {
     return $copy;
   }
 
-  /****************************************************************************/
-
-  /**
-   * Set tag.
-   *
-   * @param string $tag
-   *
-   * @return self
-   */
-  public function setTag(string $tag = NULL) : self {
-    $this->tag = $tag;
-
-    return $this;
-  }
-
-  /**
-   * Get tag.
-   *
-   * @return string|null
-   */
-  public function getTag() : ?string {
-    return $this->tag;
-  }
-
-  /**
-   * @return $this|string|null
-   */
-  public function tag() {
-    if (func_num_args() === 0) {
-      return $this->getTag();
-    }
-
-    $this->setTag(func_get_arg(0));
-
-    return $this;
-  }
-
-  /****************************************************************************/
-
   /**
    * @return string
    */
-  public function open() : string {
-    if (in_array($this->getTag(), self::$selfClosing, TRUE)) {
-      return '<'.$this->getTag().' '.parent::__toString().' />';
-    } else {
-      return '<'.$this->getTag().' '.parent::__toString().'>';
-    }
-  }
-
-  /**
-   * @return string|null
-   */
-  public function close() : ?string {
-    if (in_array($this->getTag(), self::$selfClosing, TRUE)) {
-      return NULL;
-    } else {
-      return '</'.$this->getTag().'>';
-    }
-  }
-
-  /**
-   * @return string
-   */
-  public function __toString() {
-    if (in_array($this->getTag(), self::$selfClosing, TRUE)) {
-      return '<'.$this->getTag().' '.parent::__toString().' />';
-    } else {
-      return '<'.$this->getTag().' '.parent::__toString().'></'.$this->getTag().'>';
-    }
+  protected function renderAttributes() : string {
+    return parent::__toString();
   }
 
 }
