bb3b5f740a71bb6764c3df6c791efe11b2cb13ab
Christian Puchinger <puchinger@burda.com>
2022-07-14 12:42:03 +0200

    Refactor to new directory structure.

--- src/DependencyInjection/HBMTwigAttributesExtension.php
+++ src/DependencyInjection/HBMTwigAttributesExtension.php
@@ -19,8 +19,8 @@ class HBMTwigAttributesExtension extends Extension {
    */
   public function load(array $configs, ContainerBuilder $container) {
 
-    $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-    $loader->load('services.yml');
+    $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
+    $loader->load('services.yaml');
   }
 
 }
