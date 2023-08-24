c904cdf49aa919afc8296b339e61e5300e0c2048
Joachim Krempel <joachim.krempel@burda.com>
2023-08-24 16:41:39 +0200

    Fix Extension::load signature

--- src/DependencyInjection/HBMTwigAttributesExtension.php
+++ src/DependencyInjection/HBMTwigAttributesExtension.php
@@ -17,7 +17,7 @@ class HBMTwigAttributesExtension extends Extension {
   /**
    * {@inheritdoc}
    */
-  public function load(array $configs, ContainerBuilder $container) {
+  public function load(array $configs, ContainerBuilder $container): void {
 
     $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
     $loader->load('services.yaml');
