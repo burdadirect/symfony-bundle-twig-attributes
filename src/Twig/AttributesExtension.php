5f616c0149c2a21442e1703fffd02040a614c39e
Christian Puchinger <puchinger@burda.com>
2025-09-29 03:46:31 +0200

    Update AttributesExtension.php

--- src/Twig/AttributesExtension.php
+++ src/Twig/AttributesExtension.php
@@ -12,6 +12,12 @@ use Twig\TwigTest;
 
 class AttributesExtension extends AbstractExtension
 {
+
+    public const REGEX_TAG   = '([a-z]*?)';
+    public const REGEX_ID    = '(?:#([\w-]+))';
+    public const REGEX_CLASS = '(?:\.([\w-]+))';
+    public const REGEX_ATTR  = '(?:\[(?:([\w-]+)(?:=?(?:"(.*?)"))?)?])';
+
     public function __construct(private ?TranslatorInterface $translator = null)
     {
     }
@@ -28,18 +34,41 @@ class AttributesExtension extends AbstractExtension
         return [
           new TwigFunction('attributes', $this->attributes(...)),
           new TwigFunction('tag', $this->tag(...)),
+          new TwigFunction('tagParse', $this->tagParse(...)),
         ];
     }
 
     public function getFilters(): array
     {
         return [
+          new TwigFilter('tag', $this->tagFilter(...), ['is_safe' => ['html']]),
+          new TwigFilter('tagNotEmpty', $this->tagNotEmptyFilter(...), ['is_safe' => ['html']]),
+
           new TwigFilter('transTag', $this->transTag(...)),
           new TwigFilter('transAttr', $this->transAttr(...)),
         ];
     }
 
     /* FILTERS */
+
+    public function tagFilter(HtmlTag $tag = null): ?string {
+        if ($tag === null) {
+            return null;
+        }
+
+        return (string) $tag;
+    }
+
+    public function tagNotEmptyFilter(HtmlTag $tag = null): ?string {
+        if ($tag === null) {
+            return null;
+        }
+        if (count($tag->getContent()) === 0) {
+            return null;
+        }
+
+        return (string) $tag;
+    }
 
     /**
      * Translates a html tag. Translates the title attribute by default. Add more attribute keys to translate them.
@@ -89,6 +118,40 @@ class AttributesExtension extends AbstractExtension
     public function tag(string $tag = null, HtmlAttributes|array $attributes = null): HtmlTag
     {
         return new HtmlTag($tag, $attributes);
+    }
+
+    public function tagParse(string $tagString = null, HtmlAttributes|array $attributes = null): HtmlTag
+    {
+      if (!$attributes instanceof HtmlAttributes) {
+        $attributes = new HtmlAttributes($attributes);
+      }
+
+      $tagElement = $tagString;
+      if (preg_match('/^'.self::REGEX_TAG.'{1}('.self::REGEX_ID.'?'.self::REGEX_CLASS.'*'.self::REGEX_ATTR.'*)?$/', $tagString, $matches)) {
+        $tagElement = $matches[1];
+        $this->tagParseAttributes($matches[2], $attributes);
+      }
+
+      return new HtmlTag($tagElement, $attributes);
+    }
+
+    private function tagParseAttributes(string $string, HtmlAttributes $attributes): self {
+
+        if (preg_match('/^'.self::REGEX_ID.'(.*)/', $string, $matches)) {
+            $attributes->id($matches[1]);
+            return $this->tagParseAttributes($matches[2], $attributes);
+
+        }
+        if (preg_match('/^'.self::REGEX_CLASS.'(.*)/', $string, $matches)) {
+            $attributes->class($matches[1]);
+            return $this->tagParseAttributes($matches[2], $attributes);
+        }
+        if (preg_match('/^'.self::REGEX_ATTR.'(.*)/', $string, $matches)) {
+            $attributes->set($matches[1], $matches[2]);
+            return $this->tagParseAttributes($matches[3], $attributes);
+        }
+
+        return $this;
     }
 
     /* TESTS */
